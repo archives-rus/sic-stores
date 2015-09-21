2bad8712a7c6b8bd3e0e9cb4acab3fbf340744a2
Serge Blagodatskih <stikkas@yandex.ru>
2015-09-21 09:28:28 +0300

    clear master

--- src/main/resources/data-test.sql
+++
@@ -1,3 +0,0 @@
-merge into adm_user values(1, 81, 'user', upper(md5('user')), 0, 'Простой пользователь'), 
-							(2, 81, 'userb', upper(md5('userb')), 1, 'Заблокированный пользователь'),
-							(3, 81, 'admin', upper(md5('admin')), 0, 'Администратор');
