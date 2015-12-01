5c802d008366913ae9a06da9501fe6a14b307a04
Serge Basa <stikkas@yandex.ru>
2015-12-01 15:45:59 +0300

    paginator готов

--- src/main/resources/static/scripts/app/controllers/MainController.js
+++ src/main/resources/static/scripts/app/controllers/MainController.js
@@ -5,27 +5,6 @@ SP.controller('MainCtrl',
 			me.crit = criteria;
 			// результаты поиска
 			me.result = tableResult;
-			// первая ли страница
-			me.first = function () {
-				return tableResult.first === undefined || tableResult.first;
-			};
-			// последняя ли страница
-			me.last = function () {
-				return tableResult.last === undefined || tableResult.last;
-			};
-			// начальная запись
-			me.start = function () {
-				if (tableResult.numberOfElements)
-					return tableResult.number * tableResult.size + 1;
-				return "";
-			};
-			// последняя запись
-			me.end = function () {
-				var start = me.start();
-				if (start)
-					return start + tableResult.numberOfElements - 1;
-				return "";
-			};
 
 			me.loadPage = Search.loadTablePage;
 			me.viewCard = function(index) {
--- src/main/resources/static/scripts/app/directives/paginator.js
+++ src/main/resources/static/scripts/app/directives/paginator.js
@@ -2,24 +2,34 @@ SP.directive('spPaginator', function () {
 	return {
 		templateUrl: 'partials/directives/paginator.html',
 		link: function (scope, el, attr) {
+			// первая ли страница
 			scope.first = function () {
-
+				var data = scope.data;
+				return data.first === undefined || data.first;
 			};
+			// последняя ли страница
 			scope.last = function () {
-
+				var data = scope.data;
+				return data.last === undefined || data.last;
 			};
+			// начальная запись
 			scope.start = function () {
-
+				var data = scope.data;
+				if (data.numberOfElements)
+					return data.number * data.size + 1;
+				return "";
 			};
+			// последняя запись
 			scope.end = function () {
-
-			};
-			scope.loadPage = function(page){
-
+				var start = scope.start();
+				if (start)
+					return start + scope.data.numberOfElements - 1;
+				return "";
 			};
 		},
 		scope: {
-			data: '='
+			data: '=',
+			load: '&'
 		}
 	};
 });
