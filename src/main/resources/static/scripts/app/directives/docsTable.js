34cdd8716ecc8d5ee9eca0b201ed8da3f5a325f0
Serge Basa <stikkas@yandex.ru>
2015-12-09 17:26:12 +0300

    работаю над созданием новой карточки

--- src/main/resources/static/scripts/app/directives/docsTable.js
+++ src/main/resources/static/scripts/app/directives/docsTable.js
@@ -2,10 +2,14 @@
  * Таблица для состава документов, сведений о награждениях и свединий о загранкомандировках
  */
 SP.directive('spDocsTable', function ($rootScope) {
+	function setOrder(data) {
+		data.forEach(function (it, i) {
+			it.sort = i;
+		});
+	}
 	return {
 		templateUrl: 'partials/directives/docs-table.html',
 		link: function (scope) {
-			scope.docTypes = $rootScope.docTypes;
 			scope.getText = function (id) {
 				if (scope.typeData)
 					for (var i = 0; i < scope.typeData.length; ++i) {
@@ -13,6 +17,41 @@ SP.directive('spDocsTable', function ($rootScope) {
 							return scope.typeData[i].fullValue;
 					}
 			};
+			var current = undefined;
+			scope.add = function () {
+				scope.data.push({sort: scope.data.length});
+			};
+			scope.up = function () {
+				if (current !== undefined && current !== 0) {
+					var tmp = scope.data.splice(current--, 1)[0];
+					scope.data.splice(current, 0, tmp);
+					setOrder(scope.data);
+				}
+			};
+			scope.down = function () {
+				if (current !== undefined && current !== scope.data.length - 1) {
+					var tmp = scope.data.splice(current++, 1)[0];
+					scope.data.splice(current, 0, tmp);
+					setOrder(scope.data);
+				}
+			};
+			scope.remove = function (index) {
+				scope.data.splice(index, 1);
+				var max = scope.data.length;
+				if (index < max)
+					current = index;
+				else if (max > 0)
+					current = max - 1;
+				else
+					current = undefined;
+			};
+			scope.setCurrent = function (index) {
+				current = index;
+			};
+			scope.isCurrent = function (index) {
+				return current === index;
+			};
+
 		},
 		scope: {
 			data: '=', // Object
--- src/main/resources/static/scripts/app/directives/namesTable.js
+++ src/main/resources/static/scripts/app/directives/namesTable.js
@@ -1,18 +1,48 @@
 /** 
- * Дериктива элемента 'Наименование организации и её переименования'
+ * Директива элемента 'Наименование организации и её переименования'
  */
 SP.directive('spNamesTable', function () {
+	function setOrder(data) {
+		data.forEach(function (it, i) {
+			it.sort = i;
+		});
+	}
 	return {
 		templateUrl: 'partials/directives/names-table.html',
 		link: function (scope) {
-			scope.add = function() {
-
+			var current = undefined;
+			scope.add = function () {
+				scope.data.push({sort: scope.data.length});
 			};
-			scope.up = function() {
-
+			scope.up = function () {
+				if (current !== undefined && current !== 0) {
+					var tmp = scope.data.splice(current--, 1)[0];
+					scope.data.splice(current, 0, tmp);
+					setOrder(scope.data);
+				}
 			};
-			scope.down = function() {
-
+			scope.down = function () {
+				if (current !== undefined && current !== scope.data.length - 1) {
+					var tmp = scope.data.splice(current++, 1)[0];
+					scope.data.splice(current, 0, tmp);
+					setOrder(scope.data);
+				}
+			};
+			scope.remove = function (index) {
+				scope.data.splice(index, 1);
+				var max = scope.data.length;
+				if (index < max)
+					current = index;
+				else if (max > 0)
+					current = max - 1;
+				else
+					current = undefined;
+			};
+			scope.setCurrent = function (index) {
+				current = index;
+			};
+			scope.isCurrent = function (index) {
+				return current === index;
 			};
 		},
 		scope: {
@@ -20,6 +50,5 @@ SP.directive('spNamesTable', function () {
 			edit: '=' // Boolean
 		}
 	};
-
 });
 
